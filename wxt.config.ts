4a322b73319bdd4d7be23210d9717eabbf6a52f4
hokupod <hokupod@outlook.com>
2026-03-04 17:56:51 +0900

    chore: configure extension icons from public assets

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,9 +7,19 @@ export default defineConfig({
     name: 'SafeInsert',
     description: 'IME確定による誤送信を避ける安全入力サイドパネル',
     version: '0.1.0',
+    icons: {
+      16: 'icon.png',
+      32: 'icon.png',
+      48: 'icon.png',
+      128: 'icon.png'
+    },
     permissions: ['storage', 'tabs'],
     action: {
-      default_title: 'SafeInsert'
+      default_title: 'SafeInsert',
+      default_icon: {
+        16: 'icon.png',
+        32: 'icon.png'
+      }
     },
     side_panel: {
       default_path: 'sidepanel.html'
